00525c7c11359ae55a7b2a7e07989e5226cf2b0b
MayWorkman <may.workman@agapefrance.org>
2016-07-15 15:20:41 -0500

    fixed copy of question sheets controller

--- app/controllers/fe/concerns/admin/question_sheets_controller_concern.rb
+++ app/controllers/fe/concerns/admin/question_sheets_controller_concern.rb
@@ -1,92 +1,100 @@
-module Fe::AnswerSheetsControllerConcern
+module Api::V1::Fe::Admin::QuestionSheetsControllerConcern
   extend ActiveSupport::Concern
 
   begin
     included do
-      before_filter :get_answer_sheet, :only => [:edit, :show, :send_reference_invite, :submit]
+      before_filter :check_valid_user
+      before_filter :get_question_sheet, :only => [:show, :archive, :unarchive, :destroy, :edit, :update, :duplicate]
     end
   rescue ActiveSupport::Concern::MultipleIncludedBlocks
   end
 
-  # list existing answer sheets
+  # list of all questionnaires/forms to edit
+  # GET /question_sheets
   def index
+    @active_question_sheets = Fe::QuestionSheet.active.order('label')
+    @archived_question_sheets = Fe::QuestionSheet.archived.order('label')
 
-    @answer_sheets = answer_sheet_type.order('created_at')
-
-    # drop down of sheets to capture data for
-    @question_sheets = Fe::QuestionSheet.order('label').collect {|s| [s.label, s.id]}
-    render json: { answer_sheets: @answer_sheets, question_sheets: @question_sheets }
+    render json: { active: @active_question_sheets, archived: @archived_question_sheets }
   end
 
-  def create
-    @question_sheet = Fe::QuestionSheet.find(params[:question_sheet_id])
-    @answer_sheet = @question_sheet.answer_sheets.create
-    render( { json: { answer_sheet: @answer_sheet } , status: :created } )
+  def archive
+    @question_sheet.update_attribute(:archived, true)
+    render nothing: true
   end
 
-  # display answer sheet for data capture (page 1)
-  def edit
-    @presenter = Fe::AnswerPagesPresenter.new(self, @answer_sheet, params[:a])
-    unless @presenter.active_answer_sheet.pages.present?
-
-      render_error("No questions for this form.")
-    else
-      @elements = @presenter.questions_for_page(:first).elements
-      @page = @presenter.pages.first
-      render json: {page: @page, elements: @elements}
-    end
+  def unarchive
+    @question_sheet.update_attribute(:archived, false)
+    render nothing: true
   end
 
-  # display captured answers (read-only)
+  def duplicate
+    new_question_sheet = @question_sheet.duplicate
+    render json: new_question_sheet
+  end
+
+  # GET /question_sheets/1
   def show
-    @question_sheet = @answer_sheet.question_sheet
-    pf = Fe.table_name_prefix
-    @elements = @question_sheet.pages.collect {|p| p.elements.includes(:pages).order("#{pf}pages.number,#{pf}page_elements.position").all}.flatten
-    questions = Fe::QuestionSet.new(@elements, @answer_sheet)
-    questions.set_filter(get_filter)
-    @elements = questions.elements.group_by{ |e| e.pages.first }
-    render json: { answers: @elements }
+    render json: @question_sheet
   end
 
-  def send_reference_invite(reference = nil)
-    @reference = reference || @answer_sheet.reference_sheets.find(params[:reference_id])
-    if params[:reference]
-      reference_params = params.fetch(:reference)[@reference.id.to_s].permit(:relationship, :title, :first_name, :last_name, :phone, :email, :is_staff)
+  # create sheet with inital page, redirect to show
+  # POST /question_sheets
+  def create
+    @question_sheet = Fe::QuestionSheet.new_with_page
 
-      @reference.update_attributes(reference_params)
-    end
-    if @reference.valid?
-      @reference.send_invite(request.host)
-    end
+#    respond_to do |format|
+#      if @question_sheet.save
+#        format.html { redirect_to fe_admin_question_sheet_path(@question_sheet) }
+#        format.xml  { head :created, :location => admin_question_sheet_path(@question_sheet) }
+#      else
+#        format.html { render :action => "new" }
+#        format.xml  { render :xml => @question_sheet.errors.to_xml }
+#      end
+#    end
   end
 
-  def submit
-    return false unless validate_sheet
-    render_error("form submitted")
+
+  # display sheet properties panel
+  # GET /question_sheets/1/edit
+  def edit
+
+#    respond_to do |format|
+#      format.js
+#    end
+  end
+
+  # save changes to properties panel (label, language)
+  # PUT /question_sheets/1
+  def update
+    params.require(:fe_question_sheet).permit!
+
+#    respond_to do |format|
+#      if @question_sheet.update_attributes(params[:fe_question_sheet])
+#        format.html { redirect_to fe_admin_question_sheet_path(@question_sheet) }
+#        format.js
+#        format.xml  { head :ok }
+#      else
+#        format.html { render :action => "edit" }
+#        format.js { render :action => "error.rjs"}
+#        format.xml  { render :xml => @question_sheet.errors.to_xml }
+#      end
+#    end
+  end
+
+  # mark sheet as destroyed
+  # DELETE /question_sheets/1
+  def destroy
+    @question_sheet.destroy
+
+#    respond_to do |format|
+#      format.html { redirect_to fe_admin_question_sheets_path }
+#      format.xml  { head :ok }
+#    end
   end
 
   protected
-
-  # extending classes can override this to set a questions filter
-  # see Fe::QuesitonSet#set_filter for more details
-  def get_filter
-  end
-
-  def answer_sheet_type
-    return Fe::ReferenceSheet if params[:controller] == "fe/reference_sheets"
-    (params[:answer_sheet_type] || Fe.answer_sheet_class || 'AnswerSheet').constantize
-  end
-
-  def get_answer_sheet
-    @answer_sheet = answer_sheet_type.find(params[:id])
-  end
-
-  def validate_sheet
-    unless @answer_sheet.completely_filled_out?
-      @presenter = Fe::AnswerPagesPresenter.new(self, @answer_sheet, params[:a])
-      render 'incomplete'
-      return false
-    end
-    return true
+  def get_question_sheet
+    @question_sheet = Fe::QuestionSheet.find(params[:id])
   end
 end
